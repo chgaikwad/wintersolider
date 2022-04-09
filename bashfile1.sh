3be0f73995525f2046275ad248af1bf4a0677546
user1 <user1@gmail.com>
2022-04-09 17:48:21 +0530

    updating git updated

--- bashfile1.sh
+++ bashfile1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Learning Git"
-git init
-git add
-git status
-git commit -m "<your massege>"
-git push origin branch
+#git init
+#git add
+#git status
+#git commit -m "<your massege>"
+#git push origin branch
+
+echo "Updating this file with git commands"
